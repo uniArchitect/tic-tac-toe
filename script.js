5d8a23dd2fe5a3b5e28a5df9eec934058b134d10
uniArchitect <hancho93@gmail.com>
2022-07-13 23:41:27 -0400

    created tic tac toe grid within the gamePage function and basic styles

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 const gamePage = (() => {
     // Create container div
     const container = document.createElement('div');
-
     container.classList.add('container');
-
     document.body.appendChild(container);
 
     // Create Game Board
     const gameBoard = document.createElement('div');
+    container.appendChild(gameBoard).className = 'game-board';
 
-    gameBoard.classList.add('game-board');
+    // Divide Game Board into 9 square grid
+    defineBoard = (rows, cols) => {
+        gameBoard.style.setProperty('--grid-rows', rows);
+        gameBoard.style.setProperty('--grid-cols', rows);
+        for(i = 0; i < (rows * cols); i++) {
+            let gameSquare = document.createElement('div');
+            gameBoard.appendChild(gameSquare).className = "game-square";
+        }
+    }
 
-    container.appendChild(gameBoard);
+    defineBoard(3, 3);
 
     return {container, gameBoard};
 })();
