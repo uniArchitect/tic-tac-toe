53cc949ebb6f3ff4a8a1ad337e2ea39dc6c9fa75
uniArchitect <hancho93@gmail.com>
2022-07-13 23:20:15 -0400

    created gameBoard div as a child to container

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-// Create a new container div
-const CONTAINER_DIV = (() => {
+// Create HTML components
+const gamePage = (() => {
+    // Create container div
     const container = document.createElement('div');
 
     container.classList.add('container');
 
     document.body.appendChild(container);
 
-    return {container};
+    // Create Game Board
+    const gameBoard = document.createElement('div');
+
+    gameBoard.classList.add('game-board');
+
+    container.appendChild(gameBoard);
+
+    return {container, gameBoard};
 })();
