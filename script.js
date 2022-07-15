7fbd1ea7ddb4e9b0828b1b69a60ff24b5ba1b134
uniArchitect <hancho93@gmail.com>
2022-07-15 08:48:24 -0400

    Add: cross variable for gameAction, along with SVG file into cross.innerHTML

--- script.js
+++ script.js
@@ -27,14 +27,24 @@ const gamePage = (() => {
 
 const gameAction = (() => { 
     let gameSquare = document.querySelector('.game-square');
+
+    // Define X Node
+    const cross = document.createElement('div').innerHTML = 'X';
+    cross.innerHTML = `
+    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+        <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+    </svg>
+    `
+
+    // Define Circle Node
     const circle = document.createElement('div')
     circle.innerHTML = `
     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
     </svg>
     `
-    const cross = document.createElement('div').innerHTML = 'X';
 
+    // Event: Make a move 'O' or 'X'
     changeMark = (element) => {
         element.appendChild(circle).className = 'circle-mark';
     }
