8197f1baf83e24f7cd30054f214a0286b009d4c9
uniArchitect <hancho93@gmail.com>
2022-07-15 08:24:34 -0400

    circle.createElement needed to be a node and not a string. Separated circle.innerHTML from the orginal function to fix the problem

--- script.js
+++ script.js
@@ -27,16 +27,21 @@ const gamePage = (() => {
 
 const gameAction = (() => { 
     let gameSquare = document.querySelector('.game-square');
-    const circle = document.createElement('div').innerHTML = 'O';
+    const circle = document.createElement('div')
+    circle.innerHTML = `
+    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+        <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+    </svg>
+    `
     const cross = document.createElement('div').innerHTML = 'X';
-    
-    changeMark = (element) => {
-        element.appendChild(circle).className = 'circle';
-    }
 
+    changeMark = (element) => {
+        element.appendChild(circle).className = 'circle-mark';
+    }
+    
     const circleMove = document.querySelector('.game-board').addEventListener('click', (e) => {
-        console.log(e.target);
-        gameSquare.changeMark(e.target);
+        // console.log(e.target);
+        changeMark(e.target);
     })
     
     return {circleMove, gameSquare, circle, cross, changeMark}
