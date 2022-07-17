ddd53bdffae8820dcef4b81a4aab7451becfb9b1
uniArchitect <hancho93@gmail.com>
2022-07-16 22:12:01 -0400

    FEAT: added winning combinations with winningCombo variable within gameAction

--- script.js
+++ script.js
@@ -30,21 +30,20 @@ const gameAction = (() => {
     const gameSquare = document.querySelectorAll('.game-square');
     let circleMove
 
-    // Define X Node
-    const cross = document.createElement('div');
-    cross.innerHTML = `
-    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-    </svg>
-    `
-
-    // Define Circle Node
-    const circle = document.createElement('div')
-    circle.innerHTML = `
-    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
-    </svg>
-    `
+    // Winning Combinations
+    const winningCombos = [
+        // Horizontal
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        // Vertical
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        // Diagonal
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
 
     // Event: Make a move 'O' or 'X'
     changeMark = (element, currentMove) => {
@@ -87,6 +86,8 @@ const gameAction = (() => {
     swapTurns = () => {
         circleMove = !circleMove;
     }
+
+
     
     return {gameSquare, circle, cross, changeMark}
 })();
