6549f28b0b5dfd4c4052222d47f756ddf9f6f1d6
uniArchitect <hancho93@gmail.com>
2022-07-17 22:51:34 -0400

    FEAT: each game-square class is changed with every changeMark event

--- script.js
+++ script.js
@@ -50,6 +50,11 @@ const gameAction = (() => {
 
     // Event: Make a move 'O' or 'X'
     changeMark = (element, currentMove) => {
+        if(currentMove.classList = 'cross') {
+            element.classList.add('X')
+        } else if(currentMove.classList = 'circle') {
+            element.classList.add('O')
+        }
         element.appendChild(currentMove);
     }
 
@@ -58,7 +63,8 @@ const gameAction = (() => {
         gameSquare.addEventListener('click', (e) => {
             // console.log(e.target);
             // Define X Node
-            const cross = document.createElement('div');
+            const cross = document.createElement('div')
+            cross.classList.add('cross')
             cross.innerHTML = `
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
@@ -66,6 +72,7 @@ const gameAction = (() => {
             `
             // Define Circle Node
             const circle = document.createElement('div')
+            circle.classList.add('circle')
             circle.innerHTML = `
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
@@ -86,8 +93,12 @@ const gameAction = (() => {
             // Enables the ternary operator (? :) in currentMove to switch conditions
             swapTurns();
 
-            // checkWinner();
-            
+
+
+            // if(checkWinner(currentMove)) {
+            //     console.log('winner')
+            // }
+
             // once:true limits EventListener click to be run only once in each instance
         }, {once:true});
     })
@@ -114,14 +125,31 @@ const gameAction = (() => {
     }
 
     // Check for winning combination
-    // checkWinner = () => {
-    //     if(circleArray.includes(...winningCombos) == 'true') {
+    // checkWinner = (winningCombos) => {
+    //     if(circleArray.includes(winningCombos) == 'true') {
     //         console.log('O Wins!')
-    //     } else if (crossArray.includes(...winningCombos) == 'true') {
+    //     } else if (crossArray.includes(winningCombos) == 'true') {
     //         console.log('X Wins')
     //     }
     // }
+
+    // function checkWinner(currentMove) {
+    //     return winningCombos.some(combinations => {
+    //         return combinations.every(index => {
+    //             return gameSquare[index].classList.contains(currentMove)
+    //         })
+    //     })
+    // }
+
+    function checkCrossWinner() {
+        return winningCombos.some(combinations => {
+            return combinations.every(index => {
+                return gameSquare[index].classList.contains()
+            })
+        })
+    }
     
+
     return {gameSquare, changeMark, circleArray, crossArray, winningCombos}
 })();
 
