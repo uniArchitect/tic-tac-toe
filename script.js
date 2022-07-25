026eeb23ccd179008018f3acca60985362b85f1f
uniArchitect <hancho93@gmail.com>
2022-07-24 22:51:03 -0400

    STYLE: Adjusted styling to all game squares and restart button

--- script.js
+++ script.js
@@ -187,8 +187,8 @@ const gameAction = (() => {
             cross.classList.add('cross')
             // cross.setAttribute('id', 'cross')
             cross.innerHTML = `
-            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+            <svg style="width:7em;height:7em" viewBox="0 0 24 24">
+                <path fill="white" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
             </svg>
             `
             // Define Circle Node
@@ -196,8 +196,8 @@ const gameAction = (() => {
             circle.classList.add('circle')
             // circle.setAttribute('id', 'circle')
             circle.innerHTML = `
-            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+            <svg style="width:6em;height:6em" viewBox="0 0 24 24">
+                <path fill="white" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
             </svg>
             `
             // Decides which move is up next (crossMove by default is undefined)
